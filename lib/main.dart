3b4c6c888604a9273ca0d10aa8bcd91fc607ff02
Maleiming <maleimings@gmail.com>
2023-01-05 19:55:20 +0800

    Sign-in page
    add sign-in page

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter ToDos Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter ToDos Demo'),
     );
   }
 }
@@ -48,17 +48,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _onSignInClicked() {
+
   }
 
   @override
@@ -93,23 +85,31 @@ class _MyHomePageState extends State<MyHomePage> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            const TextField(keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(10.0),
+              icon: Icon(Icons.person),
+              labelText: 'User Name',
+              helperText: 'Input your user name here'
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            autofocus: false),
+            const TextField(keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(10.0),
+                    icon: Icon(Icons.lock),
+                    labelText: 'Password',
+                    helperText: 'Input your password here'
+                ),
+                autofocus: false),
+            Padding(padding: const EdgeInsets.only(top: 20.0),
+                child: ElevatedButton(child: const Text("Sign in"), onPressed: (){
+
+            },))
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
